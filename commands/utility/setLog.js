fbea109abcd593497f4d3a55d59f85cf172934d9
SeSe008 <s.radenba@gmail.com>
2024-04-09 20:43:02 +0200

    Fixed commands, added permission to Bot Owner

--- commands/utility/setLog.js
+++ commands/utility/setLog.js
@@ -1,14 +1,26 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const global = require('../../global.js');
 
+const botOwnerId = '813744649440722956';
+
+
+function isAdminOrOwner(member) {
+    return member.permissions.has(PermissionFlagsBits.Administrator) || member.id === botOwnerId;
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('setlog')
         .setDescription('Set new log channel.')
-        .addChannelOption(option => option.setName('channel').setDescription('The channel to log timeouts in.'))
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+        .addChannelOption(option => option.setName('channel').setDescription('The channel to log timeouts in.')),
         
     async execute(interaction) {
+
+        if (!isAdminOrOwner(interaction.member)) {
+            await interaction.reply('You do not have permission to use this command.');
+            return;
+        }
+
         const channel = interaction.options.getChannel('channel');
         if (!channel) {
             return interaction.reply({ content: `No channel specified, the current channel is ${global.logChannel}`, ephemeral: true });
