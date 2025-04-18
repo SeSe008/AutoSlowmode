b5de9b97325ebca98b110414c01b40237a75fb28
SeSe008 <sebastian.radenbach@hg-gym.de>
2025-04-18 00:51:19 -0300

    Backup and load guild config

--- src/global.js
+++ src/global.js
@@ -1,6 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const backupFilePath = path.join(__dirname, '../guilds_backup.json');
+
 let guilds = [];
 let messages = {};
 let logChannel = {};
+
+// Save a persistent backup of configurations from guilds
+function saveBackup() {
+    const backupData = {
+        guilds,
+        messages,
+        logChannel
+    };
+    fs.writeFileSync(backupFilePath, JSON.stringify(backupData, null, 2));
+}
+
+function loadBackup() {
+    if (fs.existsSync(backupFilePath)) {
+        const backupData = JSON.parse(fs.readFileSync(backupFilePath));
+        guilds = backupData.guilds || [];
+        messages = backupData.messages || {};
+        logChannel = backupData.logChannel || {};
+    }
+}
+
+// Load
+loadBackup();
+
+// Save
+process.on('exit', saveBackup);
+process.on('SIGINT', () => { saveBackup(); process.exit(); });
+process.on('SIGTERM', () => { saveBackup(); process.exit(); });
 
 module.exports = {
     timeoutLength: {},
