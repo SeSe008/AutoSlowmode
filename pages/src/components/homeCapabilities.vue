bd188b6ae02246578607e676669deba925728efa
Se <s.radenba@gmail.com>
2026-05-13 22:42:31 +0200

    pages: oops #2

---
+++ pages/src/components/homeCapabilities.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="capabilities">
+    <h2>Capabilities</h2>
+    <div class="capability">
+      <div class="desc">
+        <h3>Spam Protection</h3>
+        <span>
+          Automatic spam protection.<br />When a user sends two messages in a specific time, they
+          get a timeout.
+        </span>
+      </div>
+      <img src="@/assets/images/timeout.png" alt="Timeout" />
+    </div>
+    <div class="capability">
+      <div class="desc">
+        <h3>DM Blocker</h3>
+        <span>
+          Blocks direct messages for all members.<br />Uses the new Security Actions feature every
+          24 hours, so you do not have to worry about manually setting it.
+        </span>
+      </div>
+      <img src="@/assets/images/dm_block.png" alt="Timeout" />
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+#capabilities {
+  display: flex;
+  flex-direction: column;
+  gap: 2.5vh;
+}
+
+h2 {
+  color: rgb(var(--color6));
+  font-size: xx-large;
+  text-align: center;
+}
+
+.capability {
+  padding: 1rem 5rem;
+  background-color: rgba(var(--color4), 0.3);
+  color: rgb(var(--color6));
+  gap: 1rem;
+  justify-content: center;
+}
+
+@media screen and (min-width: 1024px) {
+  .capability {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr;
+  }
+
+  .capability > .desc {
+    padding-left: 5rem;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .capability {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.capability > img {
+  flex-grow: 0;
+  height: auto;
+  width: auto;
+  max-width: 100%;
+  align-self: center;
+  border-radius: 0.75rem;
+  border: 1px solid rgb(var(--color5));
+  object-fit: contain;
+}
+</style>
---
+++ pages/src/components/homeLinks.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="links">
+    <a href="https://github.com/SeSe008/">
+      <i class="pi pi-github"></i>
+      <span>SeSe008</span>
+    </a>
+    <a href="https://discord.com/users/813744649440722956">
+      <i class="pi pi-discord"></i>
+      <span>se008</span>
+    </a>
+    <a href="mailto:s.radenba@gmail.com">
+      <i class="pi pi-envelope"></i>
+      <span>s.radenba@gmail.com</span>
+    </a>
+  </div>
+</template>
+
+<style lang="css" scoped>
+#links {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2rem;
+  flex-wrap: wrap;
+  padding: 2.5rem;
+  background-color: rgba(var(--color4), 0.3);
+}
+
+#links > a {
+  text-decoration: none;
+  color: rgb(var(--color6));
+  padding: 1rem;
+  border: 1px solid rgb(var(--color6));
+  border-radius: 0.5rem;
+  background-color: transparent;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  outline: none;
+}
+</style>
